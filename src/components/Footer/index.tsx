17ed77b4120ca3696264d141c874a23bdb1d2979
Redho Arifin <redhoarifin2017@gmail.com>
2024-07-02 14:42:40 +0900

    add url github

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
         <div className="bg-[#ee5449] min-h-44 bottom-3"></div>
         <div className="bg-slate-900 min-h-10 bottom-3">
           <div className="container">
-            <div className="text-slate-400 py-4">© 2024. Development by <a className="underline" href="https://github.com/henrykong" target="_blank">Redho Arifin</a>.</div>
+            <div className="text-slate-400 py-4">© 2024. Development by <a className="underline" href="https://github.com/redhocode" target="_blank">Redho Arifin</a>.</div>
           </div>
         </div>
       </>
