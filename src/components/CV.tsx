241d81ea7cd1a078b2b5a2affb40266f753603d0
Redho Arifin <redhoarifin2017@gmail.com>
2024-08-14 08:58:01 +0700

    update CV

--- src/components/CV.tsx
+++ src/components/CV.tsx
@@ -10,15 +10,14 @@ export function MovingButtonCV() {
         borderRadius="1.75rem"
         className="bg-white dark:bg-slate-700 text-black dark:text-white border-neutral-200 dark:border-slate-800 py-2"
       >
-        <Link href="https://replay.dropbox.com/share/MaHv5VGYPIvG9d8x" target="_blank">
-        <div className="flex flex-row gap-1">
-
-        <DownloadIcon className="w-4 h-4 animate-bounce" />
-        <span className="text-sm">
-
-          Download CV
-        </span>
-        </div>
+        <Link
+          href="https://drive.google.com/file/d/1-4mZnWj0La0f2lpaOLJfJ3FhqXkKgcpS/view?usp=sharing"
+          target="_blank"
+        >
+          <div className="flex flex-row gap-1">
+            <DownloadIcon className="w-4 h-4 animate-bounce" />
+            <span className="text-sm">Download CV</span>
+          </div>
         </Link>
       </Button>
     </div>
